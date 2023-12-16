2488099de6db78495dcca6c02d67854c900a1fb5
LopPlop <sedakovzaeba123@gmail.com>
2023-12-16 22:54:27 +0700

    Добавил страницу с товарами

---
+++ ShoppingCart/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Data.Repository;
+
+namespace ShoppingCart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductRepository _productRepository;
+        private readonly CategoryRepository _categoryRepository;
+
+        public HomeController(ILogger<ProductController> logger, IProductRepository repository, ICategoryRepository category)
+        {
+            _logger = logger;
+            _productRepository = (ProductRepository)repository;
+            _categoryRepository = (CategoryRepository)category;
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Category = _categoryRepository.GetListAsync().Result;
+            return View(_productRepository.GetListAsync().Result);
+        }
+    }
+}
